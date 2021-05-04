aaeac422260774931288c67d0d02e1c0d5c5fd4c
Cristobal <79225325+Patitotective@users.noreply.github.com>
2021-05-04 15:53:04 -0500

    Update __init__.py

--- PREFS/__init__.py
+++ PREFS/__init__.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python
 import ast
+import os
 
 class PREFS(object):
     """docstring for PREFS"""
-    def __init__(self, prefs, filename = "prefs", separator = "=", interprete = False):
+    def __init__(self, prefs, filename = "prefs", separator = "=", ender = "\n", interpret = False):
         super(PREFS, self).__init__()
         self.prefs = prefs
         self.filename = filename
         self.separator = separator
-        self.interprete = interprete
+        self.ender = ender
+        self.interpret = interpret
 
         self.ReadPrefs()
         
@@ -15,20 +18,32 @@ class PREFS(object):
         try:
             prefsTXT = open(f"{self.filename}.txt", "r")
             
-            #Do something with the file
             content = {}
             lines = prefsTXT.readlines()
-            e = 0
-            for line in lines:
-                lines[e] = line.replace("\n", "")
-                e += 1
 
-            for line in lines:
-                #print(line)
-                if not self.interprete:
-                    content[line.split(self.separator)[0]] = line.split(self.separator)[1]
-                elif self.interprete:
-                    content[line.split(self.separator)[0]] = ast.literal_eval(line.split(self.separator)[1])
+            #Do something with the file
+            if len(lines) > 1:
+                e = 0
+                for line in lines:
+                    lines[e] = line.replace(self.ender, "")
+                    e += 1
+
+                for line in lines:
+                    #print(line)
+                    if not self.interpret:
+                        content[line.split(self.separator, 1)[0]] = line.split(self.separator, 1)[1]
+                    elif self.interpret:
+                        content[line.split(self.separator, 1)[0]] = ast.literal_eval(line.split(self.separator, 1)[1])
+
+            elif len(lines) == 1:
+                line = lines[0].split(self.ender)
+                line.pop()
+                for i in line:
+
+                    if not self.interpret:
+                        content[i.split(self.separator, 1)[0]] = i.split(self.separator, 1)[1]
+                    elif self.interpret:
+                        content[i.split(self.separator, 1)[0]] = ast.literal_eval(i.split(self.separator, 1)[1])
 
             #print(content)
             prefsTXT.close()
@@ -40,10 +55,10 @@ class PREFS(object):
             except TypeError:
                 self.CreatePrefs(self.prefs)
 
-    def CreatePrefs(self, prefsDictionarie):
+    def CreatePrefs(self, prefs):
         prefsTXT = open(f"{self.filename}.txt","w+")
-        for i in prefsDictionarie.items():
-            prefsTXT.write(f"{i[0]}={i[1]}\n")
+        for i in prefs.items():
+            prefsTXT.write(f"{i[0]}={i[1]}{self.ender}")
 
         prefsTXT.close()
 
@@ -55,8 +70,22 @@ class PREFS(object):
 
         text = ""
         for item in content.items():
-            text += f"{item[0]}{self.separator}{item[1]}\n"
+            text += f"{item[0]}{self.separator}{item[1]}{self.ender}"
 
         prefsTXT = open(f"{self.filename}.txt","w+")
         prefsTXT.write(text)
         prefsTXT.close()
+
+        self.ReadPrefs()
+
+    def ReWritePrefs(self, prefs):
+        if os.path.exists(f"{self.filename}.txt"):
+            os.remove(f"{self.filename}.txt")
+            self.CreatePrefs(prefs)
+
+        self.ReadPrefs()
+            
+    def ChangeFilename(self, filename):
+        self.filename = filename
+
+        self.ReadPrefs()
