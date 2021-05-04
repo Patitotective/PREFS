8cf253bda6202d511fff651584319232f937b9d6
Cristobal <79225325+Patitotective@users.noreply.github.com>
2021-05-04 15:53:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,24 @@ classifiers = [
   'Programming Language :: Python :: 3'
 ]
  
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name='PREFS',
-  version='0.0.45',
+  version='0.0.65',
   description='A simple program that creates, read and write prefs',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/Patitotective/PREFS',  
   author='Cristobal Riaga',
   author_email='cristobalriaga@gmail.com',
   license='MIT', 
   classifiers=classifiers,
   keywords='prefs', 
   packages=find_packages(),
-  install_requires=[''] 
+  install_requires=[''], 
+
+  long_description_content_type='text/markdown'
 )
