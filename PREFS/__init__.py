5e11a86c8923a97dfafa20e09b26da0923e656b1
Cristobal <cristobalriaga@gmail.com>
2021-06-05 21:02:54 -0500

    Added dictionary and path support

--- PREFS/__init__.py
+++ PREFS/__init__.py
@@ -4,13 +4,14 @@ import os
 
 class PREFS(object):
     """docstring for PREFS"""
-    def __init__(self, prefs, filename = "prefs", separator = "=", ender = "\n", interpret = False):
+    def __init__(self, prefs, filename = "prefs", separator = "=", ender = "\n", interpret = False, dictionary = False):
         super(PREFS, self).__init__()
         self.prefs = prefs
         self.filename = filename
         self.separator = separator
         self.ender = ender
         self.interpret = interpret
+        self.dictionary = dictionary
 
         self.ReadPrefs()
         
@@ -22,30 +23,32 @@ class PREFS(object):
             lines = prefsTXT.readlines()
 
             #Do something with the file
-            if len(lines) > 1:
-                e = 0
-                for line in lines:
-                    lines[e] = line.replace(self.ender, "")
-                    e += 1
+            if not self.dictionary:
+                if len(lines) > 1:
+                    e = 0
+                    for line in lines:
+                        lines[e] = line.replace(self.ender, "")
+                        e += 1
 
-                for line in lines:
-                    #print(line)
-                    if not self.interpret:
-                        content[line.split(self.separator, 1)[0]] = line.split(self.separator, 1)[1]
-                    elif self.interpret:
-                        content[line.split(self.separator, 1)[0]] = ast.literal_eval(line.split(self.separator, 1)[1])
+                    for line in lines:
+                        if not self.interpret:
+                            content[line.split(self.separator, 1)[0]] = line.split(self.separator, 1)[1]
+                        elif self.interpret:
+                            content[line.split(self.separator, 1)[0]] = ast.literal_eval(line.split(self.separator, 1)[1])
 
-            elif len(lines) == 1:
-                line = lines[0].split(self.ender)
-                line.pop()
-                for i in line:
+                elif len(lines) == 1:
+                    line = lines[0].split(self.ender)
+                    line.pop()
+                    for i in line:
 
-                    if not self.interpret:
-                        content[i.split(self.separator, 1)[0]] = i.split(self.separator, 1)[1]
-                    elif self.interpret:
-                        content[i.split(self.separator, 1)[0]] = ast.literal_eval(i.split(self.separator, 1)[1])
+                        if not self.interpret:
+                            content[i.split(self.separator, 1)[0]] = i.split(self.separator, 1)[1]
+                        elif self.interpret:
+                            content[i.split(self.separator, 1)[0]] = ast.literal_eval(i.split(self.separator, 1)[1])
 
-            #print(content)
+            elif self.dictionary:
+                content = ast.literal_eval(lines[0])
+
             prefsTXT.close()
             return content
 
@@ -56,9 +59,18 @@ class PREFS(object):
                 self.CreatePrefs(self.prefs)
 
     def CreatePrefs(self, prefs):
-        prefsTXT = open(f"{self.filename}.txt","w+")
-        for i in prefs.items():
-            prefsTXT.write(f"{i[0]}={i[1]}{self.ender}")
+        try:
+            prefsTXT = open(f"{self.filename}.txt","w+")
+        except FileNotFoundError:
+            os.mkdir(self.filename.split("/")[0])
+            prefsTXT = open(f"{self.filename}.txt","w+")
+
+        if not self.dictionary:
+            for i in prefs.items():
+                prefsTXT.write(f"{i[0]}={i[1]}{self.ender}")
+
+        elif self.dictionary:
+            prefsTXT.write(str(prefs))
 
         prefsTXT.close()
 
@@ -68,9 +80,13 @@ class PREFS(object):
         content = self.ReadPrefs()
         content[pref] = value
 
-        text = ""
-        for item in content.items():
-            text += f"{item[0]}{self.separator}{item[1]}{self.ender}"
+        if not self.dictionary:
+            text = ""
+            for item in content.items():
+                text += f"{item[0]}{self.separator}{item[1]}{self.ender}"
+
+        elif self.dictionary:
+            text = str(content)
 
         prefsTXT = open(f"{self.filename}.txt","w+")
         prefsTXT.write(text)
@@ -78,10 +94,19 @@ class PREFS(object):
 
         self.ReadPrefs()
 
-    def ReWritePrefs(self, prefs):
+    def ReWritePrefs(self, prefs = None):
         if os.path.exists(f"{self.filename}.txt"):
             os.remove(f"{self.filename}.txt")
-            self.CreatePrefs(prefs)
+            if not self.dictionary:
+                try:
+                    self.CreatePrefs(prefs)
+                except AttributeError:
+                    self.CreatePrefs(self.prefs)
+            elif self.dictionary:
+                if not isinstance(prefs, dict):
+                    self.CreatePrefs(self.prefs)
+                if isinstance(prefs, dict):
+                    self.CreatePrefs(prefs)
 
         self.ReadPrefs()
             
@@ -89,3 +114,7 @@ class PREFS(object):
         self.filename = filename
 
         self.ReadPrefs()
+
+    def DeleteFile(self):
+        if os.path.exists(f"{self.filename}.txt"):
+            os.remove(f"{self.filename}.txt")
--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 from setuptools import setup, find_packages
- 
-classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
-]
- 
 from os import path
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-  name='PREFS',
-  version='0.0.65',
-  description='A simple program that creates, read and write prefs',
-  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='https://github.com/Patitotective/PREFS',  
-  author='Cristobal Riaga',
-  author_email='cristobalriaga@gmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords='prefs', 
-  packages=find_packages(),
-  install_requires=[''], 
+    name='PREFS',
+    version='0.0.65',
+    description='A simple program that creates, read and write prefs',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
 
-  long_description_content_type='text/markdown'
+    url='https://github.com/Patitotective/PREFS',
+    author='Cristobal Riaga',
+    author_email='cristobalriaga@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='prefs',
+    packages=find_packages(),
+    install_requires=[''],
+
+    long_description_content_type='text/markdown'
 )
